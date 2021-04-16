41b8d00a07fce0e76a393aed64587976a2db043b
AndreyKashlaty <15andrey07@gmail.com>
2021-04-16 11:30:58 +0300

    Creating 3.3 HerosInventory

---
+++ Chapter 3/3 HerosInventory/cmake-build-debug/CMakeFiles/3_HerosInventory.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "3_HerosInventory.exe"
+  "3_HerosInventory.exe.manifest"
+  "3_HerosInventory.pdb"
+  "CMakeFiles/3_HerosInventory.dir/main.cpp.obj"
+  "lib3_HerosInventory.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/3_HerosInventory.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
