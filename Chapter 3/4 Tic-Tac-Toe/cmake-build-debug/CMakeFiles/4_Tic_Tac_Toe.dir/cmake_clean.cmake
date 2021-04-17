c3951ed50eb7b7e07a0e3beb796d56fdf039a103
AndreyKashlaty <15andrey07@gmail.com>
2021-04-17 20:13:41 +0300

    finished 3.3 and create 3.4 Tic-Tac-Toe

---
+++ Chapter 3/4 Tic-Tac-Toe/cmake-build-debug/CMakeFiles/4_Tic_Tac_Toe.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "4_Tic_Tac_Toe.exe"
+  "4_Tic_Tac_Toe.exe.manifest"
+  "4_Tic_Tac_Toe.pdb"
+  "CMakeFiles/4_Tic_Tac_Toe.dir/main.cpp.obj"
+  "lib4_Tic_Tac_Toe.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/4_Tic_Tac_Toe.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
